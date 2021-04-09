ed87e5b15fae6f40dbc3bb7413ea0fb5a5d906a8
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-04-08 18:02:13 -0700

    vt - changed some more text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <Container>
       App developed for CS148 S21. Check out the source code{" "}
-      <a href="https://github.com/vincentktieu101/ReactCRAStarterCode">here</a>
+      <a href="https://github.com/vincentktieu101/ReactGoogleAuthStarter">here</a>
       .
       <br />
       <br />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,7 +20,7 @@ export default function NavBar(props) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">ReactAuthStarter</Navbar.Brand>
+        <Navbar.Brand href="/">ReactGoogleAuthStarter</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
