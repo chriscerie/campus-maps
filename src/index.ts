03219042925f1615c887562dbf78eb5b069f2ce7
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-06 17:02:06 -0700

    Update app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import * as path from 'path';
 
 const app = express();
 const port = 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/express_backend', (_, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-app.listen(process.env.PORT || port, () => console.log(`Running on port ${port}`));
+// Send all other requests back to React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client/build/index.html'));
+});
+
+app.listen(process.env.PORT || port, () =>
+  console.log(`Running on port ${port}`)
+);
