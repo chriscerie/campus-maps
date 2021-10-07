fc1f3c40a9f35cdc72f5268e3499e3d39d21ff7f
Christopher Chang <chriscerie@gmail.com>
2021-10-06 18:51:26 -0700

    Fix deployment (#29)
    * Edit scripts
    
    * Remove engine
    
    * Update path

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/express_backend', (_, res) => {
   res.send({ express: 'EXPRESS BACKEND IS CONNECTED TO REACT' });
@@ -13,7 +13,7 @@ app.get('/express_backend', (_, res) => {
 
 // Send all other requests back to React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
