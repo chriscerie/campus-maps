44530cc1b531ccd91ab7a6a8cb1ac98c3bfee045
Christopher Chang <chriscerie@gmail.com>
2021-10-10 16:35:45 -0700

    Edit testing (#32)
    * Add dependencies
    
    * Update testing
    
    * Remove import

---
+++ src/App.test.ts
@@ -0,0 +1,10 @@
+import supertest from 'supertest';
+import express, { response } from 'express';
+
+import App from './App';
+
+describe('App', () => {
+  it('should be successful', async () => {
+    expect(true).toBe(true);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
 import express from 'express';
-import * as path from 'path';
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('/express_backend', (_, res) => {
-  res.send({ express: 'EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
-
-// Send all other requests back to React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 app.listen(port, () => console.log(`Running on port ${port}`));
