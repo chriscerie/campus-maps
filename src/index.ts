3f701372b6df98af2f3bfa9b3e146e26be885bbd
Christopher Chang <chriscerie@gmail.com>
2021-10-10 18:21:23 -0700

    Fix express app (#42)
    * Fix express app
    
    * Rename app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import express from 'express';
-
-const app = express();
+import app from './app';
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Running on port ${port}`));
