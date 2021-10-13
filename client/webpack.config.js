7dd2ea3a1d414e557c415542d9480ff3a89fb531
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-12 22:04:37 -0700

    Edit webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  rules: {
-    use: {
-      loader: 'babel-loader',
-      options: {
-        ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ]
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          options: {
+            ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ]
+          }
+        }
       }
-    }
+    ]
   }
 }
