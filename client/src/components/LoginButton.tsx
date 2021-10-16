88f286087d7fb48107d08581775150425b155e99
sarahkwon <55962587+sarahkwon@users.noreply.github.com>
2021-10-16 11:01:17 -0700

    Code for google authentication (#69)
    * sk - Add section02.md
    
    * Sk - Add section02.md
    
    * Sk - More edits to section02.md
    
    * sk - google Auth0 setup
    
    * sk - removed package-lock.json
    
    * sk - added forgotten import
    
    * sk - temporarily remove login/logout buttons
    
    * Move react-google-login to client
    
    * Remove google imports
    
    * Format
    
    * Convert to typescript
    
    * Format
    
    Co-authored-by: Christopher <51393127+chriscerie@users.noreply.github.com>

---
+++ client/src/components/LoginButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from 'react-google-login';
+import { refreshTokenSetup } from '../utils/refreshToken';
+
+const clientId =
+  '678281921235-a3c8fq4225vp8ppgjmann65pmt7plaik.apps.googleusercontent.com';
+
+function LoginButton() {
+  const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+    if ('profileObj' in res) {
+      console.log('Login Success: currentUser:', res.profileObj);
+      refreshTokenSetup(res);
+    }
+  };
+
+  const onFailure = (res: any) => {
+    console.log('Login failed: res:', res);
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
+
+export default LoginButton;
---
+++ client/src/components/LogoutButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+
+const clientId =
+  '678281921235-a3c8fq4225vp8ppgjmann65pmt7plaik.apps.googleusercontent.com';
+
+function LogoutButton() {
+  const onSuccess = () => {
+    console.log('Logout made successfully');
+  };
+
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      ></GoogleLogout>
+    </div>
+  );
+}
+
+export default LogoutButton;
