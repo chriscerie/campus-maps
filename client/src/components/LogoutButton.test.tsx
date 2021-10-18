b831af8fa34086a066abceed1e723e8400ad4b26
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-17 23:57:39 -0700

    Add tests for login/logout buttons

---
+++ client/src/components/LogoutButton.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import LogoutButton from './LogoutButton';
+
+describe('LogoutButton', () => {
+  it('should render successfully', () => {
+    render(<LogoutButton />);
+  });
+});
