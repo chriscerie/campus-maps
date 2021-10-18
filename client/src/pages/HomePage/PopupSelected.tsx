3cb9c44e0252c2fcb50667d0e5a249198301b60f
Christopher Chang <chriscerie@gmail.com>
2021-10-18 00:26:44 -0700

    Edit point of interests popup UI (#72)
    * Add absolute pathing
    
    * Add icons-material
    
    * Add components absolute pathing for jest
    
    * Add tests for login/logout buttons
    
    * Edit point of interest popup ui
    
    * Fix jest absolute import
    
    * Temporarily remove absolute import

--- client/src/pages/HomePage/PopupSelected.tsx
+++ client/src/pages/HomePage/PopupSelected.tsx
@@ -1,24 +1,56 @@
-import { Container, Paper } from '@mui/material';
+import { Button, Container, Divider, Paper } from '@mui/material';
+import { Box } from '@mui/system';
 import { Feature } from 'geojson';
+import './PopupSelected.scss';
+import CloseButton from '../../components/CloseButton';
 
-function PopupSelected(props: { selected: Feature }) {
+function PopupSelected(props: {
+  selected: Feature;
+  removeSelection: () => void;
+}) {
+  const properties = props.selected.properties;
+
   return (
-    <Container sx={{ height: '100%' }}>
-      <Paper
-        elevation={3}
-        sx={{
-          position: 'relative',
-          height: '500px',
-          width: '300px',
-          top: '50%',
-          transform: 'translateY(-50%)',
-          float: 'right',
-          zIndex: 10,
-        }}
-      >
-        {props.selected.properties != null && props.selected.properties.name}
-      </Paper>
-    </Container>
+    properties && (
+      <Container className="popup-selected-container" maxWidth="xl">
+        <Paper
+          elevation={3}
+          className="popup-selected"
+          sx={{ borderRadius: '10px', overflow: 'auto' }}
+        >
+          <Box className="header-title">
+            <h1 className="header-title-title">
+              {properties.name || 'Name unavailable'}
+            </h1>
+            <CloseButton
+              sx={{ marginLeft: 'auto' }}
+              onClick={() => {
+                props.removeSelection();
+              }}
+            />
+          </Box>
+          <Box className="popup-section" sx={{ paddingTop: 0 }}>
+            <h2 className="header-title-subheader">
+              {properties.category_en || properties.type}
+            </h2>
+            <Button
+              variant="contained"
+              disableElevation
+              fullWidth
+              sx={{
+                background: '#2266e3',
+                textTransform: 'capitalize',
+                marginTop: '1rem',
+                borderRadius: '8px',
+              }}
+            >
+              More information
+            </Button>
+          </Box>
+          <Divider variant="middle" />
+        </Paper>
+      </Container>
+    )
   );
 }
 
