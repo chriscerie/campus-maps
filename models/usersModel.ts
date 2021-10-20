25e484fc3e1bb312d3a02bab3d4ef08c74ad1bbf
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-20 11:59:21 -0700

    Config auth

--- models/usersModel.ts
+++ models/usersModel.ts
@@ -1,7 +1,22 @@
 import mongoose from 'mongoose';
 
 const usersSchema = new mongoose.Schema({
-
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  googleId: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model('User', usersSchema);
