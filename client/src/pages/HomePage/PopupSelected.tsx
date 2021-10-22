92cd6d59cf8f434a423f92fc7ed31ccaf9ff91ee
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-22 00:48:43 -0700

    Remove unneeded import

--- client/src/pages/HomePage/PopupSelected.tsx
+++ client/src/pages/HomePage/PopupSelected.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Divider, Paper } from '@mui/material';
+import { Container, Divider, Paper } from '@mui/material';
 import { Box } from '@mui/system';
 import { Feature } from 'geojson';
 import './PopupSelected.scss';
