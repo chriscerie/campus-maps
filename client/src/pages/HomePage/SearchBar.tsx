cfc37e63f1fdf2cf80c7e6ef90d243a6fd6cb7bd
Jonathan Wang <jonathan_wang@umail.ucsb.edu>
2021-10-22 13:09:26 -0700

    Edit Searchbar

--- client/src/pages/HomePage/SearchBar.tsx
+++ client/src/pages/HomePage/SearchBar.tsx
@@ -1,21 +1,18 @@
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import mapboxgl, { Map } from 'mapbox-gl';
 import { useEffect } from 'react';
+import './SearchBar.scss';
 
 function SearchBar(props: { map: Map }) {
   useEffect(() => {
     const geocoder = new MapboxGeocoder({
       // Initialize the geocoder
-      accessToken: mapboxgl.accessToken, // Set the access token
+      accessToken: 'pk.eyJ1IjoiY2hyaXNjZXJpZSIsImEiOiJja3VvcXBiaGExcG5vMnFtYjhnc3gxcGprIn0.eX9g2ClfVBqYEvecwIPLYw', // Set the access token
       mapboxgl: props.map, // Set the mapbox-gl instance
       marker: false, // Do not use the default marker style
-      placeholder: 'Search for places in Berkeley', // Placeholder text for the search bar
-      bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
-      proximity: {
-        longitude: -122.25948,
-        latitude: 37.87221,
-      }, // Coordinates of UC Berkeley
+      placeholder: 'Search', // Placeholder text for the search bar
     });
+    props.map.addControl(geocoder);
   }, [props.map]);
 
   return null;
