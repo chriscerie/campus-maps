fcb1a6065f0093566928b4fe636521b606d954a9
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-22 23:12:11 -0700

    Revert mongoose connection edit

--- src/app.ts
+++ src/app.ts
@@ -23,10 +23,7 @@ app.use(
 );
 
 mongoose
-  .connect(
-    process.env.MONGODB_URI ||
-      'mongodb+srv://chriscerie:Xmqxkpt3Hb9TJbR2@primary.7qkeu.mongodb.net/myFirstDatabase?retryWrites=true'
-  )
+  .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Primary')
   .then(() => console.log('Database connected'))
   .catch((err) => console.error(err));
 
