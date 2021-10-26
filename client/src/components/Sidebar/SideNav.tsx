75210779b62d1332b98aedef27fb2ad71ac719a3
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-25 23:11:06 -0700

    Clean up

--- client/src/components/Sidebar/SideNav.tsx
+++ client/src/components/Sidebar/SideNav.tsx
@@ -1,16 +1,15 @@
-import SideArrowRight from './SideArrowRight';
-import IconButton from '@mui/material/IconButton';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import List from '@mui/material/List';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LoginIcon from '@mui/icons-material/Login';
 
 function Sidenav(props: { onClick: () => void }) {
   return (
     <div className="sidebar">
-      <SideArrowRight onClick={props.onClick} />
+      <ChevronRightIcon className="side-arrow-right" onClick={props.onClick} />
 
       <List className="items">
         {['Temp', 'Sidebar'].map((text, index) => (
--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,19 +1,19 @@
-import './Sidebar.scss';
 import SideArrow from './SideArrow';
 import Sidenav from './SideNav';
-import { Component, Fragment, useState } from 'react';
+import { useState } from 'react';
+import './index.scss';
 
 function Sidebar() {
-  const [isOpen, setIsOpen] = useState(false); // state is true: open
+  const [isOpen, setIsOpen] = useState(false);
 
   const onClick = () => {
     setIsOpen(!isOpen);
   };
-  console.log(isOpen);
-  return (
-    <Fragment>
-      {isOpen ? <Sidenav onClick={onClick} /> : <SideArrow onClick={onClick} />}
-    </Fragment>
+
+  return isOpen ? (
+    <Sidenav onClick={onClick} />
+  ) : (
+    <SideArrow onClick={onClick} />
   );
 }
 
