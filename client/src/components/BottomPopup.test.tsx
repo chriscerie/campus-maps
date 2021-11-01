1b3137e9423f44904f623ffbb068c3f15f9a7fb2
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-10-31 22:06:59 -0700

    Mock use-gesture

--- client/src/components/BottomPopup.test.tsx
+++ client/src/components/BottomPopup.test.tsx
@@ -1,6 +1,10 @@
 import ShallowRenderer from 'react-test-renderer/shallow';
 import BottomPopup from './BottomPopup';
 
+jest.mock('@use-gesture/react', () => ({
+  useDrag: jest.fn().mockReturnValue(() => ({})),
+}));
+
 describe('BottomPopup', () => {
   it('should render successfully', () => {
     const renderer = ShallowRenderer.createRenderer();
