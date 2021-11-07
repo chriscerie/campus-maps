07b9905945bf1b560f5fb2988081f3b57a6667d4
Christopher Chang <chriscerie@gmail.com>
2021-11-07 01:33:39 -0800

    Update api structure and use database for locations page (#143)
    * Move api structure to router and update locations schema
    
    * Update auth uri to match api
    
    * Use database for locations name and type
    
    * Remove location tests
    
    * Update PhotoHeader test
    
    * Format

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,36 +1,26 @@
 import passport from 'passport';
 import express from 'express';
 
-const mockApp = express();
+const router = express.Router();
 
-function auth(app: typeof mockApp): void {
-  app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email'],
-    })
-  );
+router.get(
+  '/v1/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/auth/google' }),
-    (req, res) => {
-      res.redirect('/');
-    }
-  );
-
-  app.get('/api/v1/current-user', (req, res) => {
-    if (req.user) {
-      res.status(200).send(req.user);
-    } else {
-      res.status(404).send(null);
-    }
-  });
-
-  app.post('/api/v1/logout', (req, res) => {
-    req.logout();
+router.get(
+  '/v1/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: 'api/v1/auth/google' }),
+  (req, res) => {
     res.redirect('/');
-  });
-}
+  }
+);
 
-export default auth;
+router.post('/v1/auth/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+export default router;
