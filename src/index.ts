a0eeb6a261fb1389399379bb87f8612dcbc926a4
Christopher Chang <chriscerie@gmail.com>
2021-11-08 18:48:32 -0800

    Add jest-mongodb and locations tests (#147)
    * Move mongoose connection to index to prevent connecting during test
    
    * Add jest-mongodb and add locations tests
    
    * Remove db
    
    * Disconnect mongoose after tests

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import app from './app';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Running on port ${port}`));
+
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Primary')
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.error(err));
