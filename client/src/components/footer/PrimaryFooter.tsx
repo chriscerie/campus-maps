04640a26728c94b146a5b3b48571c077e39d3ed1
Jason Em <jasonem@umail.ucsb.edu>
2021-11-10 14:06:35 -0800

    Edit Footer

--- client/src/components/footer/PrimaryFooter.tsx
+++ client/src/components/footer/PrimaryFooter.tsx
@@ -4,21 +4,26 @@ function PrimaryFooter() {
   return (
     <div className="footer-1">
       {/*<h1>LOGO GOES HERE</h1>*/}
-      
+
       <div>
         <h1>LOGO GOES HERE</h1>
       </div>
-      
-      <div className="links">
-        <a className="link" href="/loc/3588023830">LINK 1</a>
-        <a className="link" href='/loc/3588023830'>LINK 2</a>
-        <a className="link" href='/loc/3588023830'>LINK 3</a>
-        <a className="link" href='/loc/3588023830'>LINK 4</a>
-      </div>
-      <p>
-        bottom text
-      </p>
 
+      <div className="links">
+        <a className="link" href="/loc/3588023830">
+          LINK 1
+        </a>
+        <a className="link" href="/loc/3588023830">
+          LINK 2
+        </a>
+        <a className="link" href="/loc/3588023830">
+          LINK 3
+        </a>
+        <a className="link" href="/loc/3588023830">
+          LINK 4
+        </a>
+      </div>
+      <p>bottom text</p>
     </div>
   );
 }
