f65220d139b6eeaf56b53891abce1a69130a23c1
Jason Em <jasonem@umail.ucsb.edu>
2021-11-18 22:06:14 -0800

    Add AboutPage

--- client/src/pages/AboutPage/index.tsx
+++ client/src/pages/AboutPage/index.tsx
@@ -3,9 +3,28 @@ import './index.scss';
 function AboutPage() {
   return (
     <div>
-      <h1>Campus Maps about page</h1>
-      <div>pic can go here (if needed)</div>
-      <div>Whatever needs to be put under</div>
+      <div className="about-us-title">Campus Maps about page</div>
+      <div className="about-us-pic">Picture can go here if needed</div>
+      <div className="about-us-body">
+        <div className="about-us-body-parts">
+          <div className="about-us-body-top">Top</div>
+          <div className="about-us-body-middle">text</div>
+        </div>
+        <div className="about-us-body-parts">
+          <div className="about-us-body-top">Top</div>
+          <div className="about-us-body-middle">text</div>
+        </div>
+        <div className="about-us-body-parts">
+          <div className="about-us-body-top">
+            test test test test test test test test test test test
+          </div>
+          <div className="about-us-body-middle">
+            test test test test test test test test test test test test test
+            test test test test test test test test test test test tests tests
+            tests tests tests tests tests tests tests
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
