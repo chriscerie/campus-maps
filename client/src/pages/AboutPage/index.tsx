ffeea277aa7ed1d55414517ae22828770c9df3cc
jasonem225 <59809622+jasonem225@users.noreply.github.com>
2021-12-03 13:25:14 -0800

    Add AboutPage (#170)
    * Add AboutPage
    
    * Add AboutPage
    
    * Edit AboutPage
    
    * Edit AboutPage Info
    
    * Change Footer Links
    
    * Edit AbotuPage
    
    * Edit AboutPage
    
    * Edit AboutPage
    
    * Rid last line in About Page
    
    * Delete Test Lines
    
    Co-authored-by: Christopher <51393127+chriscerie@users.noreply.github.com>

---
+++ client/src/pages/AboutPage/index.tsx
@@ -0,0 +1,33 @@
+import './index.scss';
+
+function AboutPage() {
+  return (
+    <div className="about-us-all">
+      <div className="about-us-title">Where Maps meet Campuses</div>
+      <div className="about-us-text">
+        We envisioned a campus where students would no longer spend hours going
+        around campus just to find their classes. That's why we made this to
+        help you find your classes with ease.
+      </div>
+      <div className="about-us-body">
+        <div className="about-us-body-parts">
+          <div className="about-us-body-top">UCSB CS 148</div>
+          <div className="about-us-body-middle">12pm t06-campusmaps</div>
+        </div>
+        <div className="about-us-body-parts">
+          <div className="about-us-body-top">Members:</div>
+          <div className="about-us-body-middle">
+            Christopher Chang<br></br>
+            Janet Zhou<br></br>
+            Jason Em <br></br>
+            Jonathan Wang<br></br>
+            Max Binham<br></br>
+            Sarah Kwon
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutPage;
