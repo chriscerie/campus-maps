707b79dca7898e8c4607df925fe35b91813b4b99
Max Bonham <mpbonham@ucsb.edu>
2021-12-03 22:24:03 -0800

    Create share feature in location page (#199)
    * Created Share Feature
    
    * Link to write review page in add photos button
    
    Co-authored-by: Christopher <51393127+chriscerie@users.noreply.github.com>

--- client/src/pages/LocationPage/LeftColumnDetails.tsx
+++ client/src/pages/LocationPage/LeftColumnDetails.tsx
@@ -7,6 +7,8 @@ import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
 import ScreenShareOutlinedIcon from '@mui/icons-material/ScreenShareOutlined';
 import type { LocationType } from '../../types/LocationType';
+import SharePopup from './SharePopup';
+import { useState } from 'react';
 
 function LeftColumnDetais({
   locationInfo,
@@ -15,6 +17,14 @@ function LeftColumnDetais({
   locationInfo: LocationType | null;
   id: string;
 }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div id="location-page-left-column">
       <div id="location-page-left-buttons-container">
@@ -34,24 +44,31 @@ function LeftColumnDetais({
           </Link>
         </div>
         <div className="location-page-left-button">
-          <Button
-            variant="outlined"
-            color="inherit"
-            startIcon={<CameraAltOutlinedIcon />}
-            sx={{ textTransform: 'none', fontSize: '1em' }}
+          <Link
+            to={`/write-review/${id}`}
+            style={{ textDecoration: 'none', color: '#000' }}
           >
-            Add Photo
-          </Button>
+            <Button
+              variant="outlined"
+              color="inherit"
+              startIcon={<CameraAltOutlinedIcon />}
+              sx={{ textTransform: 'none', fontSize: '1em' }}
+            >
+              Add Photo
+            </Button>
+          </Link>
         </div>
         <div className="location-page-left-button">
           <Button
             variant="outlined"
             color="inherit"
+            onClick={handleOpen}
             startIcon={<ScreenShareOutlinedIcon />}
             sx={{ textTransform: 'none', fontSize: '1em' }}
           >
             Share
           </Button>
+          <SharePopup onClose={handleClose} isOpen={isOpen} />
         </div>
       </div>
 
