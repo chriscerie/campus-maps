97799f182c48142f1acf094ad25761cd42b4bbbc
Christopher <51393127+chriscerie@users.noreply.github.com>
2021-12-03 23:57:24 -0800

    Fix tests

--- client/src/pages/RoomPage/PhotoHeader.test.tsx
+++ client/src/pages/RoomPage/PhotoHeader.test.tsx
@@ -4,9 +4,7 @@ import PhotoHeader from './PhotoHeader';
 describe('PhotoHeader', () => {
   it('should render successfully', () => {
     const renderer = ShallowRenderer.createRenderer();
-    const tree = renderer.render(
-      <PhotoHeader name="name" id="1111" />
-    );
+    const tree = renderer.render(<PhotoHeader name="name" id="1111" />);
     expect(tree).toMatchSnapshot();
   });
 });
